799bdc5593d8a84ce0b30201f916beda0b2c1d48
Avinash Maurya <akmaurya31@gmail.com>
2020-11-18 18:18:40 +0530

    Update customer.model.js

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -226,15 +226,15 @@ let sqlquery="SELECT * FROM users INNER JOIN user_bank ON users.id=user_bank.use
     
 sql.query(sqlquery, (err, res) => {  
 //sql.query("SELECT * FROM users where email='"+`${email}`+"'", (err, res) => {    
-      console.log("m- line 351 ")
-      if (Array.isArray(res) && res.length) {
-      if (res[0].hasOwnProperty('email')) {  
-      let u_id=res[0].id;
-      console.log("m- line 355 ",u_id)
-      result(null, res);     }}
-      else{     
-       console.log("m- line 358 ")
-       result(null, res);      
+        console.log("m- line 351 ")
+          if (Array.isArray(res) && res.length) {
+          if (res[0].hasOwnProperty('email')) {  
+          let u_id=res[0].id;
+          console.log(sqlquery,u_id)
+          result(null, res);     }}
+          else{     
+           console.log(sqlquery)
+           result(null, res);      
     }  
   });
   
