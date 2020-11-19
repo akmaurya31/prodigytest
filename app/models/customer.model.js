36c9141897c00b2f7628c9fcf7bb256ec9e4c229
Avinash Maurya <akmaurya31@gmail.com>
2020-11-19 09:36:06 +0530

    Update customer.model.js

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -220,25 +220,7 @@ Customer.getFatcamm_nov = (email, result) => {
 
 };
  
-//////////////////////////////////////
-  Customer.purchase_normal  = (email, result) => {
-let sqlquery="SELECT * FROM users INNER JOIN user_bank ON users.id=user_bank.user_id where user_bank.isprimary_bank=1 and users.email='"+`${email}`+"'";
-    
-//sql.query(sqlquery, (err, res) => {  
-sql.query("SELECT * FROM users INNER JOIN user_bank ON users.id=user_bank.user_id where user_bank.isprimary_bank=1 and users.email='testing@gmail.com'", (err, res) => {    
-        console.log("m- line 351 ")
-          if (Array.isArray(res) && res.length) {
-          if (res[0].hasOwnProperty('email')) {  
-          let u_id=res[0].id;
-          console.log(sqlquery,u_id)
-          result(null, res);     }}
-          else{     
-           console.log(sqlquery)
-           result(null, res);      
-    }  
-  });
-  
-  };
+
 
 
   
